2bd35f31514fbc8b6347db52712695c818439212
bouchara0303 <austinabouchard@gmail.com>
2020-04-16 12:57:15 -0500

    Updated transaction route controller

--- src/controllers/transactionRouteController.ts
+++ src/controllers/transactionRouteController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { Resources, ResourceKey } from "../resourceLookup";
-import { TransactionPageResponse, CommandResponse, ActiveUser, Product, ProductListingPageResponse } from "./typeDefinitions";
+import { TransactionPageResponse, TransactionListing, CommandResponse, ActiveUser, Product, ProductListingPageResponse } from "./typeDefinitions";
 import * as Helper from "./helpers/routeControllerHelper";
 import * as ProductsQuery from "./commands/products/productsQuery";
 import * as ValidateActiveUser from "./commands/activeUsers/validateActiveUserCommand";
@@ -14,8 +14,6 @@ const processStartTransactionError = (error: any, res: Response): void => {
 	res.setHeader(
 		"Cache-Control",
 		"no-cache, max-age=0, must-revalidate, no-store");
-		
-		//Change the ViewNameLookUp to Transaction and make sure that the products are working currently
 
 	return res.status((error.status || 500))
 		.render(
@@ -33,15 +31,35 @@ export const start = async (req: Request, res: Response): Promise<void> => {
 		return;
 	}
 
+	let activeUserResponse: CommandResponse<ActiveUser>;
 	return ValidateActiveUser.execute((<Express.Session>req.session).id)
-	.then((): Promise<CommandResponse<Product[]>> => {
+	.then((activeUserCommandResponse: CommandResponse<ActiveUser>): Promise<CommandResponse<Product[]>> => {
+		activeUserResponse = activeUserCommandResponse;
 		return ProductsQuery.query();
 	}).then((productsCommandResponse: CommandResponse<Product[]>): void => {
 		return res.render(ViewNameLookup.Transaction,
 		<TransactionPageResponse>{
-			products: productsCommandResponse.data
+			products: productsCommandResponse.data,
+			employeeId: activeUserResponse.data!.id
 		});
 	}).catch((error: any): void => {
 		return processStartTransactionError(error, res);
 	});
 };
+
+export const checkTransactionEntry = async (req: Request, res: Response): Promise<void> => {
+	return QueryTransactionEntry.execute((<Express.Session>req.session).id)
+	.then((): void => {
+		res.sendStatus(200);
+	}).catch((): void => {
+		res.sendStatus(404);
+	});
+};
+
+export const createTransactionEntry = async (req: Request, res: Response): Promise<void> => {
+	return CreateTransactionEntry.execute(req.body)
+}
+
+export const updateTransactionEntry = async (req: Request, res: Response): Promise<void> => {
+	return saveTransactionEntry(req, res, TransactionEntryUpdateCommand.execute);
+}
