d1b4360a98eb480860fc08b64dd0d07a46c5b6a5
Lily <Lilytheexopanda@gmail.com>
2020-04-30 07:52:41 -0500

    -Checkout.js now has search functionality and Back, Finalize buttons -Checkout.css (No usage currently... was for Back and Finalize) -TypeDefinitions has soft TransactionID (must comment out later) -Checkout.ejs fixed to be correct -Transaction.ejs commented out unncessary stuff

--- src/controllers/typeDefinitions.ts
+++ src/controllers/typeDefinitions.ts
@@ -127,6 +127,7 @@ export interface TransactionPageResponse extends PageResponse {
 
 export interface CheckoutPageResponse extends PageResponse {
 	transactionEntries: TransactionEntry[];
+	//transactionId: string;
 }
 // End page response data
 
